03fa854435d271f517234bf0d85b7a49f9e80e71
ecwillis <eric@intomobile.com>
2011-08-04 01:34:41 -0700

    New Updates, Etc

--- wp-content/themes/slorebuzz/header.php
+++ wp-content/themes/slorebuzz/header.php
@@ -8,31 +8,14 @@
  * @subpackage Twenty_Ten
  * @since Twenty Ten 1.0
  */
+ 
+global $page, $paged;
+
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
-
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
-
-	?></title>
+<title><?php wp_title( '' );?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
@@ -76,13 +59,13 @@
 					</ul>
 				</div>
 				
-				<? /*<div id="subnav-div">
+				<div id="subnav-div">
 					<ul id="sub-header-nav">
 						<li id="news-link"><a href="/news">News</a></li>
 						<li id="qa-link"><a href="/questions_and_answers">Q &amp; A</a></li>
 						<li id="local-link"><a href="/local_resources">Local Resources</a></li>
 					</ul>
-				</div> */?>
+				</div>
 			</div><!-- #access -->
 		</div><!-- #masthead -->
 	</div><!-- #header -->
