1ed8163978766ccaf8ee6f5db77b33edb46550da
ecwillis <eric@intomobile.com>
2011-11-22 12:02:41 -0800

    mod header

--- wp-content/themes/slorebuzz/header.php
+++ wp-content/themes/slorebuzz/header.php
@@ -63,7 +63,7 @@ global $page, $paged;
 					<ul id="sub-header-nav">
 						<li id="news-link"><a href="/news">News</a></li>
 						<li id="qa-link"><a href="/question-and-answer/">Q &amp; A</a></li>
-						<li id="local-link"><a href="/local-resources/">Local Resources</a></li>
+						<li id="local-link"><a href="/welcome-to-san-luis-obispo/">Local Resources</a></li>
 					</ul>
 				</div>
 			</div><!-- #access -->
