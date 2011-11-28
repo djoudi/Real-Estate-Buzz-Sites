318dead9d63cd1cd5cd51efb36ffa419b15a189b
ecwillis <eric@intomobile.com>
2011-11-27 23:19:56 -0800

    tracking sbrebuzz

--- wp-content/themes/slorebuzz/splash.php
+++ wp-content/themes/slorebuzz/splash.php
@@ -2,6 +2,19 @@
 	<head>
 		<title><?php echo get_option('blogname');?></title>
 		<link rel="stylesheet" type="text/css" href="<? echo get_bloginfo('template_url'); ?>/splash.css" />
+		<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12475946-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 	</head>
 	<body>
 		<div id="mainWrapper">
@@ -13,5 +26,6 @@
 			<!--p><a href="#" id="LrnMo">Learn More &raquo;</a></p -->
 		</div>
 	</div>
+	
 	</body>
 </html>
