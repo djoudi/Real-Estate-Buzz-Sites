78469bf400c8f7154894479ef896f38317a32914
ecwillis <eric@intomobile.com>
2012-01-12 14:56:49 -0800

    re-adjust dyn-link settings

--- wp-load.php
+++ wp-load.php
@@ -19,7 +19,18 @@
  */
 
 /** Define ABSPATH as this files directory */
-define( 'ABSPATH', dirname(__FILE__) . '/' );
+//define( 'ABSPATH', dirname(__FILE__) . '/' );
+
+if(is_file('rb-config.php'))
+{
+	require('rb-config.php');
+} else if(is_file('../rb-config.php')){
+	require('../rb-config.php');
+} else {
+	die(dirname(__FILE__));
+}
+
+define( 'ABSPATH', RB_ABSPATH . '/' );
 
 error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
 
