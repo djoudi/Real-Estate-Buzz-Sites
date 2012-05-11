fb9ebb095ff8beb99ff749ad054490f3b5492c9d
James Van Lommel <jamesvl@gmail.com>
2012-05-11 11:00:39 -0700

    Link to MLS Search from WP sidebar

--- wp-content/themes/slorebuzz/sidebar.php
+++ wp-content/themes/slorebuzz/sidebar.php
@@ -1,11 +1,11 @@
 <div id="sidebar">
-	
 	<div id="search-box">
 		<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 			<div><label class="screen-reader-text" for="s">Search for:</label>
 				<input type="text" value="" name="s" id="s" class="input-text" />
 				<input type="image" src="/wp-content/themes/slorebuzz/images/search_go.jpg" id="searchsubmit" value="Search" style="vertical-align:middle;" />
 			</div>
+      <div>or <a href="/mls_search/">Search for Homes</a></div>
 		</form>
 		<div class="sb-footer"></div>
 	</div>
